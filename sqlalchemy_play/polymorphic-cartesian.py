225278d58f1c4b722d03fe382537bb7da734cb0b
Mike Baynton <mike@mbaynton.com>
2023-02-16 09:23:12 -0600

    Repro under 1.4

--- sqlalchemy_play/polymorphic-cartesian.py
+++ sqlalchemy_play/polymorphic-cartesian.py
@@ -1,6 +1,6 @@
 import sqlalchemy
 from sqlalchemy import or_
-from sqlalchemy.orm import sessionmaker, with_polymorphic
+from sqlalchemy.orm import sessionmaker, with_polymorphic, aliased
 
 from model.applications import Applications, Person
 
@@ -9,25 +9,30 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
 
     with Session() as db:
-        #apps = db.query(with_polymorphic(Applications, '*'))
-        #    .filter(or_(Applications.name.like('%test%'), Person.name.like('%test%')))
 
-        """
         # Produces FROM enhanced_applications, super_applications, applications JOIN persons ON persons.id = applications.owner_id
         apps = db.query(with_polymorphic(Applications, '*'))\
+            .join(Applications.owner)
+
+        """
+        apps = db.query(with_polymorphic(Applications, '*', aliased = True, flat = True))\
             .join(Applications.owner) \
-            .filter(or_(Applications.name.like('%test%'), Person.name.like('%test%')))
-        """
-
-        apps = db.query(with_polymorphic(Applications, '*'))\
-            .join(Person) \
             .filter(or_(Applications.name.like('%test%')))
+        """
+
 
         """
+        # This works:
         # Produces FROM applications LEFT OUTER JOIN enhanced_applications ON applications.id = enhanced_applications.application_id LEFT OUTER JOIN super_applications ON applications.id = super_applications.application_id JOIN persons
         apps = db.query(with_polymorphic(Applications, '*'))\
             .join(Person) \
             .filter(or_(Applications.name.like('%test%'), Person.name.like('%test%')))
+        """
+
+        """
+        # Produces FROM applications JOIN persons ON persons.id = applications.owner_id 
+        apps = db.query(Applications)\
+            .join(Applications.owner)
         """
 
         for a in apps:
